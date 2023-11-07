fcc0bedbe3471db281edaeac6cb5dbcff9a53d26
923-Feduș-Robert <robertfedus22@gmail.com>
2023-11-07 14:54:32 +0200

    Final implementation

--- GoKartComDia-SecurityAccessKeyGenerator/GoKartComDia-SecurityAccessKeyGenerator.cpp
+++ GoKartComDia-SecurityAccessKeyGenerator/GoKartComDia-SecurityAccessKeyGenerator.cpp
@@ -1,20 +1,34 @@
-// GoKartComDia-SecurityAccessKeyGenerator.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <Windows.h>
+
+// This key is used for encryption and it is not the final DCM SecurityAccess Key
+#define GENERATION_KEY 0x2202;
+
+// Encryption function
+uint16_t generateSecurityAccessKey(uint16_t seed)
+{
+    return seed ^ GENERATION_KEY;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    std::cin.unsetf(std::ios::dec);
+    std::cin.unsetf(std::ios::hex);
+    std::cin.unsetf(std::ios::oct);
+
+    uint16_t seed;
+
+    std::cout << "This is a key generator for DCM SecurityAccess (0x27) service implemented on GoKart" << std::endl << std::endl;
+    std::cout << "Enter the seed (in hex): ";
+    std::cin >> std::hex >> seed >> std::dec;
+
+    std::cout << "Seed: 0x" << std::hex << seed << std::dec << std::endl;
+    SetConsoleTextAttribute(hConsole, 2);
+    std::cout << "Key: 0x" << std::hex << generateSecurityAccessKey(seed) << std::dec << std::endl;
+
+    SetConsoleTextAttribute(hConsole, 15);
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
